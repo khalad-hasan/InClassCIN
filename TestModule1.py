d4aa5e059d70ecba06cf81121fa73cdc69b0f26f
khalad-hasan <khalad.hasan@ubc.ca>
2018-12-04 19:57:52 -0800

    test

---
+++ TestModule1.py
@@ -0,0 +1,10 @@
+import unittest
+from mod1 import addition as md1
+
+
+class TestAdd(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(md1(1,1),2)
+        self.assertEqual(md1(1,2),3)
+     
