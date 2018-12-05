72e4d7483e99eb3a890a67888cc5719845712b70
khalad-hasan <khalad.hasan@ubc.ca>
2018-12-04 20:14:28 -0800

    yml

--- TestModule1.py
+++ TestModule1.py
@@ -6,6 +6,6 @@ class TestAdd(unittest.TestCase):
 
     def test_addition(self):
         self.assertEqual(md1(1,1),2)
-        self.assertEqual(md1(1,2),4)
+        self.assertEqual(md1(1,2),3)
      
 unittest.main()
